893601aa52fe3f367bf067d90dc8f34e53a0a14d
InJeKim97 <injaekim97@naver.com>
2022-11-04 21:00:37 +0900

    feat/main/InJekim97 : 회원 도메인 개발

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,13 +2,24 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    /**
+     * MemberServiceImpl  ->  MemberRepository 접근 후
+     * memberRepository 변수명으로 new MemoryMemberRepository() 할당
+     * 즉 구조는 MemberServiceImpl ->  MemberRepository(interface) -> MemoryMemberRepository(class)
+     */
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
+    /**
+     * 회원 가입
+     */
     @Override
     public void join(Member member) {
         memberRepository.save(member);
     }
 
+    /**
+     * 회원 조회
+     */
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
