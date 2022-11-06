9c5ce4eba2b3a8bb1a7463302b2b25f6e7784a24
InJeKim97 <injaekim97@naver.com>
2022-11-06 19:49:03 +0900

    feat/main/InJekim97 : 관심사의 분리 (DIP, OCP 위반 X)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,12 +2,16 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    /* 해당 인터페이스만 의존 (DIP 위배 X) */
+    private final MemberRepository memberRepository;
+
     /**
-     * MemberServiceImpl  ->  MemberRepository 접근 후
-     * memberRepository 변수명으로 new MemoryMemberRepository() 할당
-     * 즉 구조는 MemberServiceImpl ->  MemberRepository(interface) -> MemoryMemberRepository(class)
+     * 생성자(Constructor)를 생성한 후 AppConfig에서 설정
+     * 즉 구조는 MemberServiceImpl -> AppConfig(생성자 주입) -> MemoryMemberRepository 할당
      */
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,16 +1,23 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    /* 해당 인터페이스만 의존 (DIP 위배 X) */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
+    /**
+     * 생성자(Constructor)를 생성한 후 AppConfig에서 설정
+     * 즉 구조는 OrderServiceImpl -> AppConfig(생성자 주입) -> memberRepository, discountPolicy 할당
+     */
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     /**
      * 주문 결과 반환
